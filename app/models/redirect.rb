47fe30eacf417bd095d07bd7507dc27185bf508f
Spencer Steffen <spencer@citrusme.com>
2011-06-02 16:58:03 -0700

    added validation and redirect test

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -1,6 +1,24 @@
 class Redirect < ActiveRecord::Base
 
-  validates :old_url, :presence => true, :uniqueness => { :case_sensitive => false }  
+  validates :old_url, :presence => true, :uniqueness => { :case_sensitive => false }
   validates :new_url, :presence => true
+  validate :validate_urls
 
+  before_validation :normalize_urls
+  
+  def normalize_urls
+    [ :old_url, :new_url ].map{|attribute| write_attribute attribute, normalize_url(send attribute) } 
+  end
+  
+  def validate_urls
+    errors.add(:base, I18n.t('duplicate_url_error')) if old_url == new_url    
+  end
+
+  private
+
+    def normalize_url(url)
+      return url if url.blank?
+      url.to_s.downcase.sub!(/^\/*/, '/')
+    end
+   
 end
--- test/unit/redirect_test.rb
+++ test/unit/redirect_test.rb
@@ -2,7 +2,43 @@ require_relative '../test_helper'
 
 class RedirectTest < ActiveSupport::TestCase
   
-  should validate_presence_of(:old_url)
-  should validate_presence_of(:new_url)
+  def setup    
+    @redirect = Redirect.new
+  end
+  
+  should "require urls to be set" do
+    assert !@redirect.valid?
+    assert !@redirect.save
+    assert @redirect.errors.include?(:old_url)
+    assert @redirect.errors.include?(:new_url)
+  end
+  
+  should "not allow old to equal new" do
+    @redirect.old_url = @redirect.new_url = "omgomg"
+    assert !@redirect.valid?
+    assert !@redirect.save
+    assert @redirect.errors.include?(:base)
+  end
+
+  context "a url with proper attributes" do
+  
+    setup do
+      @redirect.attributes = { :old_url => "booboo", :new_url => "woohoo" }
+    end
+  
+    should "ensure starting slash" do
+      assert @redirect.save
+      assert_equal @redirect.old_url, "/booboo"
+      assert_equal @redirect.new_url, "/woohoo"
+    end
+    
+    should "not duplicate starting slash" do
+      @redirect.attributes = { :old_url => "/booboo", :new_url => "/woohoo" }
+      assert @redirect.save
+      assert_equal @redirect.old_url, "/booboo"
+      assert_equal @redirect.new_url, "/woohoo"
+    end
+  
+  end
   
 end
