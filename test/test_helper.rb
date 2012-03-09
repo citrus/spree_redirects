213a7f124e24b21a6ee469bd3763c89e5514c177
Spencer Steffen <spencer@citrusme.com>
2012-03-09 10:44:31 -0800

    fix simplecov load error and remove rbx from travis

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,8 @@
-require "simplecov"
-SimpleCov.start "rails"
+begin
+  require "simplecov"
+  SimpleCov.start "rails"
+rescue LoadError => e
+end
 
 # Configure Rails Envinronment
 ENV["RAILS_ENV"] = "test"
