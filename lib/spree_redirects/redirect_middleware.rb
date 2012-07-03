f497e183fbdff1301870f17b676ae5e36fe90cc3
Michael Bianco <iloveitaly@gmail.com>
2012-07-02 22:35:25 -0400

    Fixing spree_redirects in production.
    I'm not sure if I was the only one, but spree_redirects did not work at all in production
    An exception would get raised and the redirect code wouldn't even have a chance to run before
    the 404 page was rendered. I've pushed the middleware down the stack and rearranged the code
    a bit: this works for me now

--- lib/spree_redirects/redirect_middleware.rb
+++ lib/spree_redirects/redirect_middleware.rb
@@ -6,23 +6,25 @@ module SpreeRedirects
     end
    
     def call(env)
+      # when consider_all_requests_local is false, an exception is raised for 404
+      # consider_all_requests_local should be false in a production environment
+
       begin
-        # when consider_all_requests_local is false, an exception is raised for 404
         status, headers, body = @app.call(env)
-      rescue ActionController::RoutingError => e
+      rescue Exception => e
         routing_error = e
       end
 
-      if (routing_error.present? or status == 404)
+      if routing_error.present? or status == 404
         path = [ env["PATH_INFO"], env["QUERY_STRING"] ].join("?").sub(/[\/\?\s]*$/, "").strip
 
         if url = find_redirect(path)
           # Issue a "Moved permanently" response with the redirect location
           return [ 301, { "Location" => url }, [ "Redirecting..." ] ]
         end
-      else
-        raise routing_error unless Rails.configuration.consider_all_requests_local
       end
+
+      raise routing_error if routing_error.present?
 
       [ status, headers, body ]
     end
