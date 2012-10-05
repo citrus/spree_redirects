ca92658bdbe5bb8efee35c5d46a8fbd6e0f2b1a9
geermc4 <german@3drobotics.com>
2012-10-05 15:32:02 -0700

    Adding admin option to sidebar
    Spree 1.3 no longer has a main list

--- app/overrides/spree_redirects.rb
+++ app/overrides/spree_redirects.rb
@@ -3,3 +3,9 @@ Deface::Override.new(:virtual_path  => "spree/admin/configurations/index",
                      :insert_bottom => "[data-hook='admin_configurations_menu']",
                      :partial       => "spree/admin/shared/redirect_config",
                      :disabled      => false)
+
+Deface::Override.new(:virtual_path  => "spree/admin/shared/_configuration_menu",
+                     :name          => "spree_redirect_config_opt_sidebar",
+                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
+                     :text          => "<%= configurations_sidebar_menu_item t(:redirects), admin_redirects_path %>",
+                     :disabled      => false)
