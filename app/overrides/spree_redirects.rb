ead20a06996c894ab791dbe9f43dfe90846663af
chuckguynn <chuckguynn@me.com>
2013-05-14 08:09:39 -0500

    add redirects to config menu

--- app/overrides/spree_redirects.rb
+++ app/overrides/spree_redirects.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path  => "spree/admin/configurations/index",
+Deface::Override.new(:virtual_path  => "spree/admin/shared/_configuration_menu",
                      :name          => "spree_redirect_config_opt",
-                     :insert_bottom => "[data-hook='admin_configurations_menu']",
-                     :partial       => "spree/admin/shared/redirect_config",
+                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
+                     :text          => "<%= configurations_sidebar_menu_item t(:redirects), admin_redirects_path %>",
                      :disabled      => false)
