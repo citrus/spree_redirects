4ac0800074bb3d8419838f02a89b61b009aae6f5
Isaac Freeman <isaac@resolvedigital.co.nz>
2014-10-08 15:50:07 +1300

    Compatibility with thinking_sphinx
    The Ransack .search method conflicts with a .search method in Thinking Sphinx. Using .ransack instead.

--- app/controllers/spree/admin/redirects_controller.rb
+++ app/controllers/spree/admin/redirects_controller.rb
@@ -4,13 +4,13 @@ class Spree::Admin::RedirectsController < Spree::Admin::ResourceController
     @redirect = Spree::Redirect.new
     render :layout => !request.xhr?
   end
-    
+
   private
-    
+
     def collection
       params[:q] ||= {}
       params[:q][:s] ||= "old_url asc"
-      @search = Spree::Redirect.search(params[:q])
+      @search = Spree::Redirect.ransack(params[:q])
       @collection = @search.result.page(params[:page]).per(Spree::Config[:orders_per_page])
     end
 
